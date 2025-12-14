b1c5f08ac55e4834bcaa6bfe634fe4619c647589
Chaitanya Katare <138283678+DarkWizardCK-24@users.noreply.github.com>
2025-12-14 19:23:20 +0530

    Delete OOPs/Main.py

--- OOPs/Main.py
+++
@@ -1,9 +0,0 @@
-from Day1.Enemy import Enemy
-
-enemy = Enemy()
-
-print(f"{enemy.type} has {enemy.health} health and does {enemy.damage} damage.")
-
-print(enemy.talk())
-print(enemy.walk())
-print(enemy.attack())
